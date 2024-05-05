b2186cb8fc631b483f191037d0f09c2fb3f17812
Marian Pralea <marianpralea@gmail.com>
2024-05-05 18:43:25 +0300

    Undo last change

--- server/app/main.py
+++ server/app/main.py
@@ -10,7 +10,7 @@ from . import db_config
 from . import models
 
 
-models.Base.metadata.create_all(bind=db_config.engine, checkfirst=True)
+models.Base.metadata.create_all(bind=db_config.engine)
 
 app = FastAPI()
 
